30d3abeb38bd41fc0ea0482838675c359ec59e3c
miquel@gmail.com <miquel@gmail.com>
2021-04-20 15:43:16 +0200

    middleware medio-donete

--- routes/api.php
+++ routes/api.php
@@ -11,24 +11,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Saloon Endpoints
-Route::get('saloon', [SaloonController::class, 'index']);
-Route::get('saloon/{sal_id}', [SaloonController::class, 'show']);
-Route::post('saloon', [SaloonController::class, 'create']);
-Route::put('saloon/{sal_id}', [SaloonController::class, 'update']);
+Route::group(['middleware' => 'jwt:api'], function () {
+    // Saloon Endpoints
+    Route::get('saloon', [SaloonController::class, 'index']);
+    Route::get('saloon/{sal_id}', [SaloonController::class, 'show']);
+    Route::post('saloon', [SaloonController::class, 'create']);
+    Route::put('saloon/{sal_id}', [SaloonController::class, 'update']);
 
-// Customer Endpoints
-Route::get('customer', [CustomerController::class, 'index']);
-Route::get('customer/{cus_id}', [CustomerController::class, 'show']);
-Route::post('customer', [CustomerController::class, 'create']);
-Route::put('customer/{cus_id}', [CustomerController::class, 'update']);
+    // Customer Endpoints
+    Route::get('customer', [CustomerController::class, 'index']);
+    Route::get('customer/{cus_id}', [CustomerController::class, 'show']);
+    Route::post('customer', [CustomerController::class, 'create']);
+    Route::put('customer/{cus_id}', [CustomerController::class, 'update']);
 
-// Appointment Endpoints
-Route::get('appointment/saloon/{sal_id}', [AppointmentController::class, 'indexSaloon']);
-Route::get('appointment/customer/{cus_id}', [AppointmentController::class, 'indexCustomer']);
-Route::get('appointment/{app_id}', [AppointmentController::class, 'show']);
-Route::post('appointment', [AppointmentController::class, 'create']);
-Route::put('appointment/{app_id}', [AppointmentController::class, 'update']);
+    // Appointment Endpoints
+    Route::get('appointment/saloon/{sal_id}', [AppointmentController::class, 'indexSaloon']);
+    Route::get('appointment/customer/{cus_id}', [AppointmentController::class, 'indexCustomer']);
+    Route::get('appointment/{app_id}', [AppointmentController::class, 'show']);
+    Route::post('appointment', [AppointmentController::class, 'create']);
+    Route::put('appointment/{app_id}', [AppointmentController::class, 'update']);
+    
+});
 
 // This endpoint does not need authentication.
 Route::get('public', function (Request $request) {
